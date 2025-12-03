16a863581a0e7eae2db385e9df3aa53be1050345
Caio <125697270+CaioXTSY@users.noreply.github.com>
2025-12-03 18:51:32 -0300

    feat: update allowedHosts configuration in Vite settings for flexibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,11 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   vite: {
+    server: {
+      allowedHosts: true
+    },
     preview: {
-      allowedHosts: ['dev323ast5.caiots.dev']
+      allowedHosts: true
     }
   }
 });
