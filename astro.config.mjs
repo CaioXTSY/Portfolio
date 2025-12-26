a80a9973b296391ae27f0e3dcc739f503f15e3e6
Caio <125697270+CaioXTSY@users.noreply.github.com>
2025-12-25 21:30:14 -0300

    feat(seo): add sitemap integration and meta tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://caiots.dev',
+  integrations: [tailwind(), sitemap()],
   vite: {
     server: {
       allowedHosts: true
